a4d9383f0e78d5ad1528402f882faf069e2d16e5
Kevin <pxxzhe001@gmail.com>
2019-07-29 12:16:28 +0100

    working code to output high or low

---
+++ prac1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Python Practical Template
+Keegan Crankshaw
+Readjust this Docstring as follows:
+Names: <names>
+Student Number: <studnum>
+Prac: <Prac Num>
+Date: <dd/mm/yyyy>
+"""
+
+# import Relevant Librares
+import RPi.GPIO as GPIO
+
+# Logic that you write
+def main():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(7, GPIO.IN)
+    GPIO.input(7)
+    if GPIO.input(7):
+        print("input was high")
+    else:
+        print("input was low")
+
+
+# Only run the functions if
+if __name__ == "__main__":
+    # Make sure the GPIO is stopped correctly
+    try:
+        while True:
+            main()
+    except KeyboardInterrupt:
+        print("Exiting gracefully")
+        # Turn off your GPIOs here
+        GPIO.cleanup()
+    except Exception as e:
+        GPIO.cleanup()
+        print("Some other error occurred")
+        print(e.message)
