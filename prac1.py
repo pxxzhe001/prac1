2dad50ff231d6fe70d9b480ad62eec31a958af11
Kevin <pxxzhe001@gmail.com>
2019-07-30 00:00:33 +0100

    working binary counter and one output led

--- prac1.py
+++ prac1.py
@@ -16,28 +16,44 @@ import time
 # Logic that you write
     #set pin mode to board
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.IN)
+GPIO.setup(7, GPIO.OUT)
 GPIO.setup(11, GPIO.IN) 
+GPIO.setup(13, GPIO.IN) 
 def increment(channel):
     print("button pressed")
 
 
 GPIO.add_event_detect(11, GPIO.RISING, callback=increment, bouncetime=300)
+GPIO.add_event_detect(13, GPIO.RISING, bouncetime=300)
 
 def main():
     count = 0
     #check button click
+    #loop
     while count < 8:
-        if GPIO.input(7):
-            print("input was high")
-            time.sleep(1)
-        else:
-            print("input was low")
-            time.sleep(1)
+	time.sleep(1)
+#        if GPIO.input(7):
+#            print("input was high")
+#            time.sleep(1)
+#        else:
+#            print("input was low")
+#            time.sleep(1)
         if GPIO.event_detected(11):
             print('Button pressed' + str(count))
             count += 1
-
+        if GPIO.event_detected(13):
+            print('Button pressed' + str(count))
+            count -= 1
+	#set negative count to max(7)
+	if count < 0:
+	    count = 7
+	#change count to binary
+	bcount = format(count,'03b')
+	print(bcount)
+	pout0 = int(bcount[0])
+	pout1 = int(bcount[1])
+	pout2 = int(bcount[2])
+	GPIO.output(7,pout0)
 # Only run the functions if
 if __name__ == "__main__":
     # Make sure the GPIO is stopped correctly
